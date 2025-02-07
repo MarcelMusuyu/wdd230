27e7a1041611a44c83e36c8fe2f0ce9c51b2c4d5
marcel Musuyu <marcelmusuyu9@gmail.com>
2025-02-07 15:04:37 +0200

    Team work assignment

--- scripts/links.js
+++ scripts/links.js
@@ -1,12 +1,42 @@
 const baseURL = "https://marcelmusuyu.github.io/wdd230/";
 const linksURL = `${baseURL}/data/links.json`;
 
+const content = document.querySelector(".link");
+
 async function getLinks() {
-  const response = await fetch(linksURL);
-  const data = await response.json();
-    console.log(data);
-     displayLinks(data);
+  try {
+    const response = await fetch(linksURL);
+    if (response.ok) {
+      const weeks = await response.json();
+      displayLinks(weeks);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-getLinks();
+function displayLinks(data) {
+  //data.weeks[0].links[0].url;
+  console.log(data.weeks[0].links[0].url);
+  //   data.weeks.forEach((week) => {
+  //     console.log(week.url);
+  //   }); &
+  data.weeks.forEach((week) => {
+    // console.log(week.week);
+    const activities = document.createElement("li");
 
+    activities.setAttribute("class", "activities");
+    activities.textContent = `${week.week: }`;
+    week.links.forEach((link) => {
+      const task = document.createElement("a");
+      task.innerHTML = `${link.title}| &nbsp;`;
+      task.setAttribute("href", link.url);
+      activities.appendChild(task);
+      // console.log(`${link.title} ${link.url}`);
+    });
+    content.appendChild(activities);
+  });
+}
+getLinks();
