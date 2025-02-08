d31c70448ff74060fa8cf8291102a5258d442a92
marcel Musuyu <marcelmusuyu9@gmail.com>
2025-02-08 12:56:28 +0200

    Enhancement of Home page

--- scripts/links.js
+++ scripts/links.js
@@ -19,7 +19,7 @@ async function getLinks() {
 
 function displayLinks(data) {
   //data.weeks[0].links[0].url;
-  console.log(data.weeks[0].links[0].url);
+
   //   data.weeks.forEach((week) => {
   //     console.log(week.url);
   //   }); &
@@ -28,10 +28,10 @@ function displayLinks(data) {
     const activities = document.createElement("li");
 
     activities.setAttribute("class", "activities");
-    activities.textContent = `${week.week: }`;
+    activities.textContent = `${week.week}: `;
     week.links.forEach((link) => {
       const task = document.createElement("a");
-      task.innerHTML = `${link.title}| &nbsp;`;
+      task.innerHTML = `&nbsp;${link.title}|&nbsp;`;
       task.setAttribute("href", link.url);
       activities.appendChild(task);
       // console.log(`${link.title} ${link.url}`);
@@ -40,3 +40,40 @@ function displayLinks(data) {
   });
 }
 getLinks();
+
+// Success! We have the user's coordinates.
+const latitude = -11.66;
+const longitude = 27.47;
+
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("#weather-description");
+const apiKey = "0609f6b96504ef3e8683e2a1ba72a07f";
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(data) {
+  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+
+  const iconsrc = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  console.log(iconsrc);
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", data.weather[0].description);
+  captionDesc.textContent = data.weather[0].description;
+}
+apiFetch();
